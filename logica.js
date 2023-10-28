f39d42a07cb7cfb2b6c5d5f4d0b7841a6e9bb149
Carlos Sánchez <sanchez.carlos.1986@gmail.com>
2023-10-28 14:35:08 -0300

    feat(hola): test

--- logica.js
+++ logica.js
@@ -1,18 +1,21 @@
 const {readFile} = require('fs');
 
-const targetControls = ['EXPORT_DEFAULT', 'BAD_IMPORT'];
+const TARGET_CONTROLS = ['EXPORT_DEFAULT', 'BAD_IMPORT'];
+const TARGET_FILE = 'results.txt';
+const regExp = control => new RegExp(control, 'ig');
+const ERROR = 'TIENES CONTROLES ESTÁTICOS QUE RESOLVER';
 
-readFile('./results.txt', 'utf-8', (err, data) => {
-  for (const control of targetControls) {
-    const target = new RegExp(control, 'ig');
+readFile(`./${TARGET_FILE}`, 'utf-8', (err, data) => {
+  if (err) {
+    throw new Error(`there was an error reading the file ${TARGET_FILE}`);
+  }
 
-    const ERROR = 'TIENES CONTROLES ESTÁTICOS QUE RESOLVER';
-
-    if (target.test(data)) {
+  for (const control of TARGET_CONTROLS) {
+    if (regExp(control).test(data)) {
       console.log(`\x1b[31m ❌ ${ERROR}: [${control}] ❌ \x1b[0m`);
 
       console.log(
-        `\x1b[33m REVISA EL ARCHIVO: ${process.cwd()}/results.txt  \x1b[0m`,
+        `\x1b[33m REVISA EL ARCHIVO: ${process.cwd()}/${TARGET_FILE}  \x1b[0m`,
       );
 
       throw new Error(ERROR);
