c4317c7c43b84dfc83f119bd26ba0ff29c792dcd
Carlos Sánchez <sanchez.carlos.1986@gmail.com>
2023-10-28 12:04:43 -0300

    feat(hola): test

--- logica.js
+++ logica.js
@@ -1,17 +1,21 @@
-const fs = require('fs');
+const {readFile} = require('fs');
 
-const targetControls = ['BAD_IMPORT', 'EXPORT_DEFAULT'];
+const targetControls = ['EXPORT_DEFAULT', 'BAD_IMPORT'];
 
-fs.readFile('./results.txt', 'utf-8', (err, data) => {
+readFile('./results.txt', 'utf-8', (err, data) => {
   for (const control of targetControls) {
     const target = new RegExp(control, 'ig');
 
-    if (target.test(data)) {
-      console.error(`TIENES CONTROLES ESTÁTICOS QUE RESOLVER`);
-      console.error(`REVISA EL ARCHIVO: ${process.cwd()}/results.txt`);
+    const ERROR = 'TIENES CONTROLES ESTÁTICOS QUE RESOLVER';
 
-      // throw new Error();
-      process.exit();
+    if (target.test(data)) {
+      console.log(`\x1b[31m ❌ ${ERROR}: [${control}] ❌ \x1b[0m`);
+
+      console.log(
+        `\x1b[33m REVISA EL ARCHIVO: ${process.cwd()}/results.txt  \x1b[0m`,
+      );
+
+      throw new Error(ERROR);
     }
   }
 });
