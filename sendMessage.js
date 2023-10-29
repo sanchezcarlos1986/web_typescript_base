a611abcff9d207afa7ffba5a81345049ef8c280e
Carlos Sánchez <sanchez.carlos.1986@gmail.com>
2023-10-28 23:09:18 -0300

    feat(hola): test

--- sendMessage.js
+++ sendMessage.js
@@ -1,15 +1,30 @@
-import {getGitUserInfo} from 'git-user-info';
-import figlet from 'figlet';
-import chalk from 'chalk';
+const {getGitUserInfo} = require('git-user-info');
+const figlet = require('figlet');
 
-const SENTENCES = ['Sonreir cada día', 'Todo tiene solución', 'HAPPY CODING!'];
+const COLORS = {
+  RED: '\x1b[31m',
+  GREEN: '\x1b[32m',
+  YELLOW: '\x1b[33m',
+  BLUE: '\x1b[34m',
+};
+
+const SENTENCES = [
+  'Sonreir cada día',
+  'Ser respetuoso',
+  'Todo tiene solución',
+  'Ser feliz y agradecido',
+  'La vida es corta, disfruta',
+  'HAPPY CODING!',
+];
 
 const generateRandomMessage = () => {
-  console.log(chalk.bgWhite.bold('   RECUERDA:    '));
+  console.log(`${COLORS.YELLOW}    [RECUERDA:]    \x1b[0m`);
 
   for (const sentence of SENTENCES) {
-    console.log(chalk.bold.green(` ✔ ${sentence} `));
+    console.log(`${COLORS.GREEN}  ✔ ${sentence} \x1b[0m`);
   }
+
+  console.log(`\n\n`);
 };
 
 const sendMessage = message => {
@@ -29,7 +44,7 @@ const sendMessage = message => {
         return;
       }
 
-      console.log(chalk.blue(data));
+      console.log(data);
 
       generateRandomMessage();
     },
